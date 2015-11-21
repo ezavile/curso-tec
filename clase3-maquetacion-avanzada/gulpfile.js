6683179ac55557398d58130a8628833de994c02c
Edgar Zavala <ezavile@gmail.com>
2015-11-21 10:38:50 -0600

    Add - gulp tasks

--- clase3-maquetacion-avanzada/gulpfile.js
+++ clase3-maquetacion-avanzada/gulpfile.js
@@ -23,24 +23,41 @@ var uglify = require('gulp-uglify');
 ////////// JADE ////////////
 var jade = require('gulp-jade');
 
-///////// BORRAR LA CARPETA PUBLIC CADA VEZ QUE TRABAJE GULP ////
+///////// BORRAR LA CARPETA BUILD CADA VEZ QUE TRABAJE GULP ////
 var clean = require('gulp-rimraf');
 
 var config = {
 	styles: {
 		main: './src/styles/app.styl',
-		output: './build/css'
+		output: './build/css',
+		watch: './src/styles/*.styl'
 	},
 	htmls: {
 		main: './src/index.jade',
-		output: './build'
+		output: './build',
+		watch: './src/*.jade'
 	},
 	scripts:{
 		main: './src/scripts/app.js',
-		output: './build/js'
+		output: './build/js',
+		watch: './src/scripts/*.js'
 	}
 }
 
+gulp.task('server', function(){
+	gulp
+		.src('./build')
+		.pipe(webserver({
+			host: '0.0.0.0',
+			port: 8080
+		}));
+})
+
+gulp.task('clean', function(){
+	return gulp
+				.src('./build', {read: false})
+				.pipe(clean(-{force: true}));
+})
 
 gulp.task('build:html', function(){
 	gulp
@@ -48,7 +65,8 @@ gulp.task('build:html', function(){
 		.pipe(jade({
 			pretty: true
 		}))
-		.pipe(gulp.dest(config.htmls.output));
+		.pipe(gulp.dest(config.htmls.output))
+		.pipe(livereload());
 });
 
 
@@ -60,7 +78,8 @@ gulp.task('build:css', function(){
 			'include css':true
 		}))
 		.pipe(minifyCSS())
-		.pipe(gulp.dest(config.styles.output));
+		.pipe(gulp.dest(config.styles.output))
+		.pipe(livereload());
 });
 
 
@@ -68,5 +87,20 @@ gulp.task('build:js', function(){
 	gulp
 		.src(config.scripts.main)
 		.pipe(uglify())
-		.pipe(gulp.dest(config.scripts.output));
+		.pipe(gulp.dest(config.scripts.output))
+		.pipe(livereload());
 });
+
+// Observa los cambios tanto en HTML, CSS y JS
+gulp.task('watch', function(){
+	livereload.listen();
+	gulp.watch(config.styles.watch, ['build:css']);
+	gulp.watch(config.scripts.watch, ['build:js']);
+	gulp.watch(config.htmls.watch, ['build:html']);
+})
+
+//Tarea por default
+gulp.task('default',['clean','server','watch'], function(){
+	gulp
+		.start('build:css','build:js','build:html');
+})
